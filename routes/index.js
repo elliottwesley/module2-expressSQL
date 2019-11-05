698da9862d449e0d4d4a8339656fa1756eff713c
elliottwesley <aquariuswre@ksu.edu>
2019-11-05 09:09:53 -0600

    Edited several files, changed a few names, installed nodemon

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
-var index = require('../controllers/index');
+var landing = require('../controllers/landing');
 
 /* GET home page. */
-router.get('/', index.index);
+router.get('/', landing.get_landing);
+router.post('/', landing.submit_lead);
 
 module.exports = router;
