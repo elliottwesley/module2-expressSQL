dc407a757c336a760287b6ac8aa0f05409c4bfc4
elliottwesley <aquariuswre@ksu.edu>
2019-11-05 10:36:45 -0600

    Added the sequelize and pg libraries and created relevant files.

---
+++ config/config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  "development": {
+    "username": "wunmnvbd",
+    "password": "z84hUt_9C2D4f6LXswkMaSgcmLXgx14G",
+    "database": "wunmnvbd",
+    "host": "salt.db.elephantsql.com",
+    "dialect": "postgres",
+	"port": 5432,
+    "operatorsAliases": false
+  },
+  "test": {
+    "username": "root",
+    "password": null,
+    "database": "database_test",
+    "host": "127.0.0.1",
+    "dialect": "mysql",
+    "operatorsAliases": false
+  },
+  "production": {
+    "username": "root",
+    "password": null,
+    "database": "database_production",
+    "host": "127.0.0.1",
+    "dialect": "mysql",
+    "operatorsAliases": false
+  }
+}
