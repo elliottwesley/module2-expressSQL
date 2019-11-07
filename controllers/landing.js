227b18d4f0fb56f12f453025e97f647fb06673dd
elliottwesley <aquariuswre@ksu.edu>
2019-11-07 09:24:03 -0600

    Added a migration, a model, and additional views.
    Discovered 1 known error caused by a lack of a database to post to.
    This will be rectified later.

--- controllers/landing.js
+++ controllers/landing.js
@@ -1,10 +1,57 @@
+const models = require('../models')
 
-
+// Load the index page (landing.pug) on GET
 exports.get_landing = function(req, res, next) {
-  res.render('landing', { title: 'Express' });
+	res.render('landing', { title: 'Express' });
 }
 
+// Submit a created lead on POST
 exports.submit_lead = function(req, res, next) {
-  console.log("lead email:", req.body.lead_email);
-  res.redirect('/');
+	return models.Lead.create({
+		email: req.body.lead_email
+	}).then(lead => {
+		res.redirect('/leads');
+	})
+}
+
+// Show all leads on GET
+exports.show_leads = function(req, res, next) {
+	return models.Lead.findAll().then(leads => {
+		res.render('landing', { title: 'Express', leads: leads });
+	})
+}
+
+// Show individual lead on GET
+exports.show_lead = function(req, res, next) {
+	return models.Lead.findOne({
+		where: {
+			id: req.params.lead_id
+		}
+	}).then(lead => {
+		res.render('lead', {lead: lead});
+	});
+}
+
+// Show edit lead on GET
+exports.show_edit_lead = function(req, res, next) {
+	return models.Lead.findOne({
+		where: {
+			id: req.params.lead_id
+		}
+	}).then(lead => {
+		res.render('lead/edit_lead', {lead: lead});
+	});
+}
+
+// Edit (update) lead on POST
+exports.edit_lead = function(req, res, next) {
+	return models.Lead.update({
+		email: req.body.lead_email
+	}, {
+		where: {
+			id: req.params.lead_id
+		}
+	}).then(result => {
+		res.redirect('/lead/' + req.params.lead_id);
+	});
 }
