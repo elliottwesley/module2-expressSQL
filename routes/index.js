813a26038e1066af6e02c669c19473bb3802ddf6
elliottwesley <aquariuswre@ksu.edu>
2019-11-18 20:24:41 -0600

    Implemented hasAuth middleware and is_admin flag, and the lead routes are now protected from any visitor or user who is not the administrator.

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,8 @@ var router = express.Router();
 var landing = require('../controllers/landing');
 var user = require('../controllers/user');
 
+var {isLoggedIn, hasAuth} = require('../middleware/hasAuth.js');
+
 // Login and signup functionality.
 router.get('/login', user.show_login);
 router.get('/signup', user.show_signup);
@@ -12,14 +14,15 @@ router.post('/signup', user.signup);
 // Logout functionality.
 router.post('/logout', user.logout);
 router.get('/logout', user.logout);
-// GET home page, landing routes.
+
+// GET home page, landing routes.  First two are for visitors, rest are for administrator.
 router.get('/', landing.get_landing);
 router.post('/', landing.submit_lead);
-router.get('/leads', landing.show_leads);
-router.get('/lead/:lead_id', landing.show_lead);
-router.get('/lead/:lead_id/edit', landing.show_edit_lead);
-router.post('/lead/:lead_id/edit', landing.edit_lead);
-router.post('/lead/:lead_id/delete', landing.delete_lead);
-router.post('/lead/:lead_id/delete-json', landing.delete_lead_json);
+router.get('/leads', hasAuth, landing.show_leads);
+router.get('/lead/:lead_id', hasAuth, landing.show_lead);
+router.get('/lead/:lead_id/edit', hasAuth, landing.show_edit_lead);
+router.post('/lead/:lead_id/edit', hasAuth, landing.edit_lead);
+router.post('/lead/:lead_id/delete', hasAuth, landing.delete_lead);
+router.post('/lead/:lead_id/delete-json', hasAuth, landing.delete_lead_json);
 
 module.exports = router;
